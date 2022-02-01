b82731355c76af2c8dd919c6b2d47676c55db35c
Invisible <vitaliysomoniuk@gmail.com>
2022-02-01 16:28:34 +0200

    Added templates and their binding urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id':1, 'name':'Lets learn python 1'},
+    {'id':2, 'name':'Lets learn python 2'},
+    {'id':3, 'name':'Lets learn python 3'},
+]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
